6b1528b5c7248cfa102259d5678abc37e38706c7
jiyinyiyong <jiyinyiyong@gmail.com>
2017-09-22 20:05:27 +0800

    trying latest version

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,16 +1,19 @@
 
 (ns app.main
-  (:require (app.lib :as lib)))
+  (:require [app.lib :as lib]
+            ["hsl" :as hsl]))
 
 (def a 1)
 
 (defonce b 2)
 
 (defn main! []
-  (println "[main]: loading"))
+  (println "[main]: loading")
+  (println "calling hsl" (hsl 10 10 10)))
 
 (defn reload! []
   (println "[main] reloaded lib:" lib/c lib/d)
-  (println "[main] reloaded:" a b))
+  (println "[main] reloaded:" a b)
+  (println "calling hsl" (hsl 10 10 10)))
 
 (main!)
